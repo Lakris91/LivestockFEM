1defbc1bd4480ee7b68929f51febd12e37f62d6e
Lakris91 <lasse_krist@hotmail.com>
2017-11-03 11:14:38 +0100

    Topology to MatLab text translator component

---
+++ TopologyTranslator_v1.0.py
@@ -0,0 +1,19 @@
+﻿import rhinoscriptsyntax as rs
+i=1
+MatLabNodes=""
+for node in nodes:
+    X,Y,Z = rs.PointCoordinates(node)
+    nodesString= "X("+str(i)+",:) = ["+str(X)+" "+str(Y)+"];"
+    MatLabNodes=MatLabNodes+nodesString+"\n"
+    i+=1
+MatLabNodes=MatLabNodes+"nno="+str(i-1)
+
+i=1
+MatLabElements=""
+ESI=ElementStartIndex
+EEI=ElementEndIndex
+for j in range(len(ElementStartIndex)):
+    elementString= "T("+str(i)+",:) = ["+str(ESI[j]+1)+" "+str(EEI[j]+1)+"];"
+    MatLabElements=MatLabElements+elementString+"\n"
+    i+=1
+MatLabElements=MatLabElements+"nel="+str(i-1)
