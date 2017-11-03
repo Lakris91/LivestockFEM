6fa057292a3e537405ad1f6641fdd845e10ba637
Lakris91 <lasse_krist@hotmail.com>
2017-11-03 15:43:08 +0100

    Partly added node hinge

--- DOFSgenerator.py
+++ DOFSgenerator.py
@@ -1,6 +1,10 @@
 ﻿import rhinoscriptsyntax as rs
 ESI=ElementStartIndex
 EEI=ElementEndIndex
+nodecount=max(ElementStartIndex+ElementEndIndex)-min(ElementStartIndex+ElementEndIndex)+1
+dofcount=nodecount*3
+AHE=AddHingeElement
+AHN=AddHingeNode
 uninodes=[]
 doflist=[]
 DOFSlist=[]
@@ -21,8 +25,33 @@ for i in range(len(ESI)):
         rngstart=rngstart+3
     else:
         dofend=doflist[uninodes.index(EEI[i])]
+    
+    if len(AHE) == 0:
+        for ahn in AHN:
+            if ESI[i]==ahn:
+                dofstart[2]=0
+            if EEI[i]==ahn:
+                dofend[2]=0
+    
     DOFS=dofstart+dofend
     DOFSlist.append(DOFS)
+
+DOFSflat = [item for sublist in DOFSlist for item in sublist]
 print DOFSlist
+print DOFSflat
+#number of DOFS
 
+"""
+nd=rngstart-1
+print nd
 
+if len(AHE) == 0:
+    #Add hinge in all elements connected to node
+    for ahn in AHN:
+        print "ahn="+str(ahn)
+        print EEI
+        if not ESI.count(ahn)==0:
+            print ESI.index(ahn)
+        if not EEI.count(ahn)==0:
+            print EEI.index(ahn)
+"""
