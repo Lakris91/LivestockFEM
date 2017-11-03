7cd391a893cc0cf2e3ec046cca2e8c59d23c6fc3
Lakris91 <lasse_krist@hotmail.com>
2017-11-03 13:21:09 +0100

    Degrees of freedom component, initiation done, manipulation to be done

---
+++ DOFSgenerator.py
@@ -0,0 +1,28 @@
+﻿import rhinoscriptsyntax as rs
+ESI=ElementStartIndex
+EEI=ElementEndIndex
+uninodes=[]
+doflist=[]
+DOFSlist=[]
+rngstart=1
+for i in range(len(ESI)):
+    if uninodes.count(ESI[i])==0:
+        uninodes.append(ESI[i])
+        dofstart=range(rngstart,rngstart+3)
+        doflist.append(dofstart)
+        rngstart=rngstart+3
+    else:
+        dofstart=doflist[uninodes.index(ESI[i])]
+    
+    if uninodes.count(EEI[i])==0:
+        uninodes.append(EEI[i])
+        dofend=range(rngstart,rngstart+3)
+        doflist.append(dofend)
+        rngstart=rngstart+3
+    else:
+        dofend=doflist[uninodes.index(EEI[i])]
+    DOFS=dofstart+dofend
+    DOFSlist.append(DOFS)
+print DOFSlist
+
+
