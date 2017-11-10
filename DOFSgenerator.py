00852a0bdfa096ec7baf05a4a38e143f19848d27
Lakris91 <lasse_krist@hotmail.com>
2017-11-10 11:36:46 +0100

    DOFS component only lacking MatLab translation and visualization

--- DOFSgenerator.py
+++ DOFSgenerator.py
@@ -5,10 +5,16 @@ nodecount=max(ElementStartIndex+ElementEndIndex)-min(ElementStartIndex+ElementEn
 dofcount=nodecount*3
 AHE=AddHingeElement
 AHN=AddHingeNode
+#Repeat last element of shortest list
+if len(AHE)<>0 and len(AHN)<>0:
+    maxlen = max(len(AHE),len(AHN))
+    AHE.extend([AHE[-1]]*(maxlen-len(AHE)))
+    AHN.extend([AHN[-1]]*(maxlen-len(AHN)))
 uninodes=[]
 doflist=[]
 DOFSlist=[]
 rngstart=1
+#Define no-hinge DOFS
 for i in range(len(ESI)):
     if uninodes.count(ESI[i])==0:
         uninodes.append(ESI[i])
@@ -25,27 +31,39 @@ for i in range(len(ESI)):
         rngstart=rngstart+3
     else:
         dofend=doflist[uninodes.index(EEI[i])]
-    
-    if len(AHE) == 0:
-        for ahn in AHN:
-            if ESI[i]==ahn:
-                dofstart[2]=0
-            if EEI[i]==ahn:
-                dofend[2]=0
-    
     DOFS=dofstart+dofend
     DOFSlist.append(DOFS)
 
-DOFSflat = [item for sublist in DOFSlist for item in sublist]
-while DOFSflat.count(0)<>0:
-    m = range(1,len(DOFSflat))
-    new=min(set(m)-set(DOFSflat))
-    DOFSflat[DOFSflat.index(0)]=new
-newDOFSLIST=[]
-templist=[]
-for dofs in DOFSflat:
-    templist.append(dofs)
-    if len(templist)==6:
-        newDOFSLIST.append(templist)
-        templist=[]
-print newDOFSLIST
+if len(AHN)<>0:
+    #Add position of hinge
+    for j in range(len(DOFSlist)):
+        if len(AHE) == 0:
+            for ahn in AHN:
+                print inidof==ahn
+                if ESI[j]==ahn:
+                    DOFSlist[j][2]=0
+                if EEI[j]==ahn:
+                    DOFSlist[j][5]=0
+        else:
+            for ahe in AHE:
+                if ahe==j:
+                    for ahn in AHN:
+                        if ESI[j]==ahn:
+                            DOFSlist[j][2]=0
+                        if EEI[j]==ahn:
+                            DOFSlist[j][5]=0
+    
+    #Add hinge
+    DOFSflat = [item for sublist in DOFSlist for item in sublist]
+    while DOFSflat.count(0)<>0:
+        m = range(1,len(DOFSflat))
+        new=min(set(m)-set(DOFSflat))
+        DOFSflat[DOFSflat.index(0)]=new
+    newDOFSLIST=[]
+    templist=[]
+    for dofs in DOFSflat:
+        templist.append(dofs)
+        if len(templist)==6:
+            newDOFSLIST.append(templist)
+            templist=[]
+    DOFSlist = newDOFSLIST
