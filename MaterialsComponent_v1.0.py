bdf7c440cb6da4cfab83895cd5eaf87506dd10f9
Lakris91 <lasse_krist@hotmail.com>
2017-11-18 22:00:00 +0100

    Material component, the output now match the amount elements

--- MaterialsComponent_v1.0.py
+++ MaterialsComponent_v1.0.py
@@ -1,17 +1,19 @@
 ﻿
-if not (len(Emodul)==0 or len(SecArea)==0 or len(Inertia)==0):
+if not (len(Emodul)==0 or len(SecArea)==0 or len(Inertia)==0 or len(ElementNo)==0):
     #Repeat last element in list to get equal list lenght
-    maxlen = max(len(Emodul),len(SecArea),len(Inertia))
+    maxlen = len(ElementNo)
     SecArea.extend([SecArea[-1]]*(maxlen-len(SecArea)))
     Emodul.extend([Emodul[-1]]*(maxlen-len(Emodul)))
     Inertia.extend([Inertia[-1]]*(maxlen-len(Inertia)))
+    SecArea=SecArea[:maxlen]
+    Emodul=Emodul[:maxlen]
+    Inertia=Inertia[:maxlen]
     
-    
-    MatLabText=""
+    MatLabMaterial=""
     materials=[]
-    for i in range(len(Emodul)):
-        mate="G("+str(i+1)+",:)=["+str(Emodul[i])+" "+str(SecArea[i])+" "+str(Inertia[i])+"];"
+    for i in range(maxlen):
+        mate="G("+str(ElementNo[i]+1)+",:)=["+str(Emodul[i])+" "+str(SecArea[i])+" "+str(Inertia[i])+"];"
         materials.append("E-module="+str(Emodul[i])+" Area="+str(SecArea[i])+" Inertia="+str(Inertia[i]))
-        MatLabText=MatLabText+mate+"\n"
+        MatLabMaterial=MatLabMaterial+mate+"\n"
 else:
     print "Zero length lists do NOT work"
