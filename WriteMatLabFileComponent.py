626dd0bbd395a43ae1433954db4c73fc80f483e1
Lakris91 <lasse_krist@hotmail.com>
2017-11-18 22:55:44 +0100

    Create matlab text working

---
+++ WriteMatLabFileComponent.py
@@ -0,0 +1,15 @@
+﻿ML=open("C:/Git/LKplugin/RammeLKpluginTemplate.m","r")
+MLstring=ML.read()
+ML.close()
+
+MLstring = MLstring.replace("MatLabNodes",MatLabNodes)
+MLstring = MLstring.replace("MatLabElements",MatLabElements)
+MLstring = MLstring.replace("MatLabDOFS",MatLabDOFS)
+MLstring = MLstring.replace("MatLabMaterial",MatLabMaterial)
+MLstring = MLstring.replace("MatLabSupport",MatLabSupport)
+MLstring = MLstring.replace("MatLabNodeLoads",MatLabNodeLoads)
+MLstring = MLstring.replace("MatLabElementLoad",MatLabElementLoad)
+MLstring = MLstring.replace("MatLabElementLoad",MatLabElementLoad)
+MLstring = MLstring.replace("PlotScalingDeformation",str(PlotScalingDeformation[0]))
+MLstring = MLstring.replace("PlotScalingForces",str(PlotScalingForces[0]))
+print MLstring
