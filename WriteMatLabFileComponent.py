e7a59bf875a0e6fefbbd1a0a28a219ab32d02bf2
Lakris91 <lasse_krist@hotmail.com>
2017-12-09 13:50:20 +0100

    Write and runs matlab file to specific folder not relative folder

--- WriteMatLabFileComponent.py
+++ WriteMatLabFileComponent.py
@@ -12,4 +12,14 @@ MLstring = MLstring.replace("MatLabElementLoad",MatLabElementLoad)
 MLstring = MLstring.replace("MatLabElementLoad",MatLabElementLoad)
 MLstring = MLstring.replace("PlotScalingDeformation",str(PlotScalingDeformation[0]))
 MLstring = MLstring.replace("PlotScalingForces",str(PlotScalingForces[0]))
-print MLstring
+#print MLstring
+
+mFile=open("C:/Git/LKplugin/TempMLfile.m","w")
+mFile.write(MLstring)
+mFile.close()
+
+import os
+import subprocess
+if run:
+    subprocess.Popen("matlab -nosplash -nodesktop -r \"run C:\Git\LKplugin\TempMLfile.m\"")
+    #os.startfile("C:/Git/LKplugin/RunMLscript.bat")
