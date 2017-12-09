6545c90cecd648586d47be8d635390b0a282e5c0
Lakris91 <lasse_krist@hotmail.com>
2017-12-10 00:19:04 +0100

    Component now plots the deformation

--- WriteMatLabFileComponent.py
+++ WriteMatLabFileComponent.py
@@ -1,6 +1,8 @@
 ﻿import os
 import subprocess
 import Grasshopper as GH
+import time
+import rhinoscriptsyntax as rs
 
 def matlabIsInstalled():
     for path in os.environ["PATH"].split(";"):
@@ -31,9 +33,47 @@ mFile=open(tempFPath,"w")
 mFile.write(MLstring)
 mFile.close()
 
-if matlab_installed():
+print tempFPath
+
+resultfile=UOfolder+"result.txt"
+deformationfile=UOfolder+"deformation.txt"
+
+deformationcurves=[]
+if matlabIsInstalled():
     if run:
-        subprocess.Popen("matlab -nosplash -nodesktop -r \"run "+tempFPath+"\"")
-        #os.startfile("C:/Git/LKplugin/RunMLscript.bat")
+        subprocess.Popen("matlab -nosplash -nodesktop -minimize -r \"run "+tempFPath+"\"")
 
-
+if os.path.isfile(resultfile) and os.path.isfile(deformationfile):
+    if refresh:
+        result=open(resultfile,"r")
+        resultstring=result.read()
+        result.close()
+        resList=resultstring.split(",")
+        V=[]
+        Ru=[]
+        F1=[]
+        F2=[]
+        M=[]
+        listlist=[V,Ru,F1,F2,M]
+        listCount=0
+        for res in resList:
+            if res[:1]=="/":
+                listCount+=1
+                res=res.replace("/","")
+            listlist[listCount].append(res)
+        
+        deformation=open(deformationfile,"r")
+        defstring=deformation.read()
+        deformation.close()
+        eleList=defstring.split("_")
+        ptList=[]
+        for ele in eleList:
+            deformationpoints=[]
+            if len(ele)>0:
+                XYlist=ele.split("/")
+                Xlist=XYlist[0].split(",")
+                Ylist=XYlist[1].split(",")
+            for i in range(len(Xlist)-1):
+                deformationpoints.append(rs.AddPoint(float(Xlist[i]),float(Ylist[i]),0))
+            deformationcurves.append(rs.AddInterpCurve(deformationpoints))
+print deformationcurves
